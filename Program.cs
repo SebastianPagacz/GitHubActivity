f27c22f47b6c01bbf3d4d77ac0b383f33b5fd59f
SebastianPagacz <234595@student.uek.krakow.pl>
2025-03-24 22:41:27 +0100

    Need to think about it more

--- Program.cs
+++ Program.cs
@@ -22,24 +22,7 @@ internal class Program
             var result = await client.GetAsync(url);
             var json = await result.Content.ReadAsStringAsync();
             var deserializedData = JsonConvert.DeserializeObject<List<Activity>>(json);
-
-            var groupByActivity = from activity in deserializedData group activity by activity.Type into newGroup select newGroup;
-
-            foreach (var i in groupByActivity)
-            {
-                Console.WriteLine($"{i.Key}");
-                foreach (var j in i)
-                {
-                    Console.WriteLine($"{j.Actor.Login} : {j.CreatedAt} : {j.Repo.Name} : {j.Type}");
-                }
-            }
-
-            int countPushes = deserializedData.Count(p => p.Type == "PushEvent");
-            int countCreates = deserializedData.Count(p => p.Type == "CreateEvent");
-            int countReleases = deserializedData.Count(p => p.Type == "ReleaseEvent");
-            Console.WriteLine($"{user} pushed {countPushes} commits");
-            Console.WriteLine($"{user} created {countCreates}");
-            Console.WriteLine($"{user} realeased {countReleases} project");
+            
         }
     }
 }
